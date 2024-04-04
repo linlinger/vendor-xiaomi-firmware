68e43e7574b9d43b62f169b9b7376afeeabd0cb9
linlinger <32541118+linlinger@users.noreply.github.com>
2024-04-05 01:58:12 +0800

    Initial commit for ginkgo
    Only base structure, Go and download firmware yourself from https://xiaomifirmwareupdater.com/firmware/ginkgo/stable/V12.5.5.0.RCOCNXM/

---
+++ README.md
@@ -0,0 +1,1 @@
+# Xiaomi Device Firmwares 
---
+++ ginkgo/firmware.mk
@@ -0,0 +1,20 @@
+# Firmware
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi-firmware/ginkgo/abl.elf:install/firmware-update/abl.elf \
+    vendor/xiaomi-firmware/ginkgo/BTFM.bin:install/firmware-update/BTFM.bin \
+    vendor/xiaomi-firmware/ginkgo/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+    vendor/xiaomi-firmware/ginkgo/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+    vendor/xiaomi-firmware/ginkgo/devcfg.mbn:install/firmware-update/devcfg.mbn \
+    vendor/xiaomi-firmware/ginkgo/dspso.bin:install/firmware-update/dspso.bin \
+    vendor/xiaomi-firmware/ginkgo/hyp.mbn:install/firmware-update/hyp.mbn \
+    vendor/xiaomi-firmware/ginkgo/imagefv.elf:install/firmware-update/imagefv.elf \
+    vendor/xiaomi-firmware/ginkgo/km4.mbn:install/firmware-update/km4.mbn \
+    vendor/xiaomi-firmware/ginkgo/NON-HLOS.bin:install/firmware-update/NON-HLOS.bin \
+    vendor/xiaomi-firmware/ginkgo/qupv3fw.elf:install/firmware-update/qupv3fw.elf \
+	vendor/xiaomi-firmware/ginkgo/rpm.mbn:install/firmware-update/rpm.mbn \
+    vendor/xiaomi-firmware/ginkgo/storsec.mbn:install/firmware-update/storsec.mbn \
+    vendor/xiaomi-firmware/ginkgo/tz.mbn:install/firmware-update/tz.mbn \
+	vendor/xiaomi-firmware/ginkgo/uefi_sec.mbn:install/firmware-update/uefi_sec.mbn \
+    vendor/xiaomi-firmware/ginkgo/xbl.elf:install/firmware-update/xbl.elf \
+    vendor/xiaomi-firmware/ginkgo/xbl_config.elf:install/firmware-update/xbl_config.elf
+
